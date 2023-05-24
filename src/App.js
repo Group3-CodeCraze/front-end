43f9091c9e8ffa61742c5eb6997ba9be0b38f7f9
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-05-25 00:26:01 +0300

    Revert "finsh react home page"

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
 
-import Home from "./component/home/Home";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import  Footer from'./component/footer/Footer'
-function App()
-{
-  return(
-    <>
-    
-    
-<Navbar bg="dark" variant="dark">
-                <Container>
-                    
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">about us</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-            <Home/>
-            <Footer/>
-            
-    </>
-    
-  )
-}export default App;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
