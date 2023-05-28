260f515cb0bf64019642ecbec7a68628da609afd
Abboodo <abboodoa@gmail.com>
2023-05-28 22:34:58 +0000

    Abdallah : user Auth full with signin and signup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
-
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
-function Header (){
-    return<>
-    <Navbar bg="dark" variant="dark">
+function Header({ username }) {
+  const [loggedInUsername, setLoggedInUsername] = useState('');
+
+  useEffect(() => {
+    setLoggedInUsername(username);
+  }, [username]);
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">Task Genius</Navbar.Brand>
           <Nav className="me-auto">
@@ -13,12 +21,23 @@ function Header (){
             <Nav.Link href="/AboutUS">AboutUS</Nav.Link>
             <Nav.Link href="/RandomTask">RandomTask</Nav.Link>
             <Nav.Link href="/MyTasks">MyTasks</Nav.Link>
-            
+            {loggedInUsername ? (
+              <>
+                <Nav.Link>Hello {loggedInUsername}</Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                  Log Out
+                </Nav.Link>
+              </>
+            ) : (
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+            )}
           </Nav>
         </Container>
       </Navbar>
-    
     </>
+  );
 }
 
-export default Header
+export default Header;
