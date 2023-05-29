aaf9485f5ee5bde519285c0f66973ff81fb386b3
ramawm20 <ramawael54@gmail.com>
2023-05-29 22:27:44 +0000

    rama:add styling to the RandomTask page

--- src/components/RandomTask.js
+++ src/components/RandomTask.js
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
 import Form from 'react-bootstrap/Form';
 import TaskModel from "./TaskModel";
+import './RandomTask.css'
 
 
 function RandomTask() {
@@ -29,62 +30,71 @@ function RandomTask() {
 
 
     const handelSelectedValue = (e) => {
-        
+        const selectedOption = e.target.value;
+        let image = '';
+        image = require(`./${selectedOption}.jpg`);
+        document.body.style.backgroundImage = `url(${image})`;
+
+        document.body.style.backgroundPosition = 'center'
         const value = e.target.value;
-       
+
         setSelectedValue(value);
 
     }
 
     const genTask = (e) => {
-        if(selectedValue ===""){
+        if (selectedValue === "") {
             e.preventDefault()
             setgenerate("please select type")
-        }else{
+        } else {
             e.preventDefault()
-          
+
             generateRandom(selectedValue)
         }
-        
-          
-    
+
+
+
     }
 
     const generateRandom = (type) => {
-        
-           console.log(selectedValue)
-            const serverURL = `http://localhost:3000/randomTask/${type}`
 
-            axios.get(serverURL)
-                .then(response => {
+        console.log(selectedValue)
+        const serverURL = `http://localhost:3000/randomTask/${type}`
 
-                    console.log(response.data)
-                    setgenerate(response.data.activity)
-                })
-                .catch(error => {
-                    console.log(error)
-                })
+        axios.get(serverURL)
+            .then(response => {
+
+                console.log(response.data)
+                setgenerate(response.data.activity)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
 
 
     return (
         <>
-
             <Form onSubmit={genTask}>
-                <Form.Select onChange={handelSelectedValue} aria-label="Default select example" className="fs-3">
-                    <option value="none" >Please select type</option>
-                    <option value="music"  >music</option>
-                    <option value="education">education</option>
-                    <option value="social">social</option>
-                    <option value="recreational">recreational</option>
-                    <option value="charity">charity</option>
-                </Form.Select>
-                <Button type="submit" onClick={() => { handelshow(generate) }}>submit</Button>
+                <div className='dropdown-container'>
+
+                    <Form.Select onChange={handelSelectedValue} aria-label="Default select example" className='select'>
+                        <option value="none" >Please select type</option>
+                        <option value="music"  >music</option>
+                        <option value="education">education</option>
+                        <option value="social">social</option>
+                        <option value="recreational">recreational</option>
+                        <option value="charity">charity</option>
+                    </Form.Select>
+                    <div className="button-container">
+                        <Button className="button" type="submit" onClick={() => { handelshow(generate) }}>Submit</Button>
+                    </div>
+                </div>
+
             </Form>
 
-            <TaskModel showFlag={showFlag} handelshow={handelshow} handelclose={handelclose} generate={generate}  selectedValue={selectedValue} />
-
+            <TaskModel showFlag={showFlag} handelshow={handelshow} handelclose={handelclose} generate={generate} selectedValue={selectedValue} />
         </>
     )
 
